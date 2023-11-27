995cbdbbb411a47f6ed65e850b7574f170e61512
Mujassamar <132365259+Mujassamar@users.noreply.github.com>
2023-11-27 16:39:53 +0700

    Update app.dart
    beberapa update tambahan

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'login_page.dart';
-import 'beranda.dart';
+import 'sakukus.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/start', // Tentukan rute awal
       routes: {
         '/login': (context) => LoginPage(),
-        '/': (context) => BerandaPage(),
+        '/': (context) => Beranda2Page(),
         // Tambahkan rute lain jika diperlukan
       },
     );
