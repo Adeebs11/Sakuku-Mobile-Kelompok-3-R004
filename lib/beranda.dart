7ad9439fbe8d34234173f56ec315fd2d0432f94b
Mujassamar <132365259+Mujassamar@users.noreply.github.com>
2023-11-27 16:41:04 +0700

    Update beranda.dart
    perubahan pada page beranda

--- lib/beranda.dart
+++ lib/beranda.dart
@@ -1,65 +1,79 @@
 import 'package:flutter/material.dart';
 
 class BerandaPage extends StatelessWidget {
+  const BerandaPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 567,
-                  color: Color(0xFFA2AFD7),
+          SizedBox(height: 50.0),
+          Row(
+            children: [      
+              Text('        '),
+              Image.asset('assets/images/SakukuLogo.png'),
+              Text(
+                '                    Hai,',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 100, 96, 96),
+                  fontSize: 25,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w700,
                 ),
-                // Add other widgets here as needed
-              ],
+              ),
+            ],
+          ),
+          Text(
+            'Pengguna',
+            style: TextStyle(
+              color: Color.fromARGB(255, 58, 57, 57),
+                  fontSize: 30,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w700,
             ),
           ),
-          buildFooter(),
+          SizedBox(width: 40, height: 40),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            
+            children: [
+              ContainerWithText('Total Pemasukkan', Colors.white),
+              SizedBox(height: 25.0),
+              ContainerWithText('Detail Pemasukkan', Colors.white),
+              SizedBox(height: 25.0),
+              ContainerWithText('Total Pengeluaran', Colors.white),
+              SizedBox(height: 25.0),
+              ContainerWithText('Detail Pengeluaran', Colors.white),
+            ],
+          ),
         ],
       ),
     );
   }
+}
 
-  Widget buildFooter() {
+class ContainerWithText extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  ContainerWithText(this.text, this.color);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      height: 49,
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          buildNavItem('Beranda', Icons.home),
-          buildNavItem('Budget', Icons.attach_money),
-          buildNavItem('Transaksi', Icons.swap_horiz),
-          buildNavItem('Akun', Icons.person),
-        ],
-      ),
-    );
-  }
-
-  Widget buildNavItem(String title, IconData iconData) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          iconData,
-          size: 25,
-          color: Color(0xFF898989),
-        ),
-        SizedBox(height: 5),
-        Text(
-          title,
+      width: 350.0,
+      height: 70.0,
+      color: color,
+      child: Center(
+        child: Text(
+          text,
           style: TextStyle(
-            color: Color(0xFF898989),
-            fontSize: 11,
-            fontFamily: 'Montserrat',
-            fontWeight: FontWeight.w500,
+            color: const Color.fromARGB(255, 210, 10, 10),
+            fontSize: 18.0,
           ),
         ),
-      ],
+      ),
     );
   }
 }
