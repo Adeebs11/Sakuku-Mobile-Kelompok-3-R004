34e1ed6bc5dc56392de53e17f533615ba08ce5d6
Mujassamar <132365259+Mujassamar@users.noreply.github.com>
2023-11-27 16:42:15 +0700

    Create akun_page.dart
    penambahan menu akun

---
+++ lib/akun_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AkunPage extends StatelessWidget {
+  const AkunPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('This is a Akun Page',style: TextStyle(fontSize: 40),),
+      ),
+    );
+  }
+}
