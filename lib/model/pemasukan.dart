dcefdba1bf8a64b60f6fe8b47cce8ef5fc1d5de8
Adeebs11 <132633345+Adeebs11@users.noreply.github.com>
2023-12-11 21:58:22 +0700

    Create pemasukan.dart

---
+++ lib/model/pemasukan.dart
@@ -0,0 +1,31 @@
+class Pemasukan {
+  int? id;
+  String? kategori;
+  String? tanggal;
+  String? jumlah;
+  String? deskripsi;
+
+  Pemasukan(
+      {this.id, this.kategori, this.tanggal, this.jumlah, this.deskripsi});
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['kategori'] = kategori;
+    map['tanggal'] = tanggal;
+    map['jumlah'] = jumlah;
+    map['deskripsi'] = deskripsi;
+    return map;
+  }
+
+  Pemasukan.fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.kategori = map['kategori'];
+    this.tanggal = map['tanggal'];
+    this.jumlah = map['jumlah'];
+    this.deskripsi = map['deskripsi'];
+  }
+}
