df1c2dc68fad032b7d93817b2740448dfecdd6c2
Mujassamar <132365259+Mujassamar@users.noreply.github.com>
2023-12-12 11:27:12 +0700

    Update transaksi_provider.dart

--- lib/model/transaksi_provider.dart
+++ lib/model/transaksi_provider.dart
@@ -4,8 +4,9 @@ class Transaksi {
   final String id;
   final String kategori;
   final double jumlah;
+  final DateTime tanggal;
 
-  Transaksi({required this.id, required this.kategori, required this.jumlah});
+  Transaksi({required this.id, required this.kategori, required this.jumlah, required this.tanggal});
 }
 
 class TransaksiProvider with ChangeNotifier {
@@ -20,6 +21,7 @@ class TransaksiProvider with ChangeNotifier {
 
   void addTransaksi(Transaksi transaksi) {
     _transaksi.add(transaksi); // Perbarui total setelah menambah transaksi
+    
     notifyListeners();
   }
 
@@ -60,6 +62,108 @@ class TransaksiProvider with ChangeNotifier {
         'Jumlah transaksi untuk kategori $kategori: ${transaksiByCategory.length}');
     return transaksiByCategory.length;
   }
+
+  Transaksi? getLastIncomeByCategory(List<String> categories) {
+  List<Transaksi> incomeList = _transaksi
+      .where((transaksi) =>
+          categories.contains(transaksi.kategori) && transaksi.jumlah > 0)
+      .toList();
+
+  if (incomeList.isEmpty) {
+    return null;
+  }
+
+  incomeList.sort((a, b) => b.tanggal.compareTo(a.tanggal));
+  return incomeList.first;
+}
+
+  Transaksi? getLastExpenseByCategory(List<String> categories) {
+  List<Transaksi> incomeList = _transaksi
+      .where((transaksi) =>
+          categories.contains(transaksi.kategori) && transaksi.jumlah > 0)
+      .toList();
+
+  if (incomeList.isEmpty) {
+    return null;
+  }
+
+  incomeList.sort((a, b) => b.tanggal.compareTo(a.tanggal));
+  return incomeList.first;
+}
+
+ Transaksi? getLastTransactionByCategory(String category) {
+  List<Transaksi> filteredList =
+      _transaksi.where((transaksi) => transaksi.kategori == category).toList();
+  
+  if (filteredList.isNotEmpty) {
+    filteredList.sort((a, b) => b.tanggal.compareTo(a.tanggal));
+    return filteredList.first;
+  }
+
+  return null;
+}
+
+
+
+  Transaksi? getLargestIncomeTransaction(String category) {
+  List<Transaksi> incomeList = _transaksi
+      .where((transaksi) =>
+          transaksi.kategori == category &&
+          transaksi.jumlah > 0) // Memastikan hanya pemasukan yang diambil
+      .toList();
+
+  if (incomeList.isNotEmpty) {
+    incomeList.sort((a, b) => b.jumlah.compareTo(a.jumlah));
+    return incomeList.first;
+  }
+    return null;
+  }
+
+  Transaksi? getSmallestIncomeTransaction(String kategori) {
+  List<Transaksi> incomeTransactions =
+      _transaksi.where((t) => t.kategori == kategori).toList();
+
+  if (incomeTransactions.isEmpty) {
+    return null;
+  }
+
+  incomeTransactions.sort((a, b) => a.jumlah.compareTo(b.jumlah));
+  return incomeTransactions.first;
+}
+
+  Transaksi? getLargestExpenseTransaction(String category) {
+    List<Transaksi> expenseTransactions = _transaksi
+    .where((transaksi) => 
+        transaksi.kategori == category &&
+        transaksi.jumlah > 0) // Memastikan hanya pengeluaran yang diambil
+    .toList();
+
+    if (expenseTransactions.isEmpty) {
+      return null;
+    }
+
+    expenseTransactions.sort((a, b) => b.jumlah.compareTo(a.jumlah));
+    return expenseTransactions.first;
+  }
+
+  Transaksi? getSmallestExpenseTransaction(String kategori) {
+  List<Transaksi> expenseTransactions = _transaksi
+      .where((transaksi) =>
+          transaksi.kategori == kategori && transaksi.jumlah > 0)
+      .toList();
+
+  print('Jumlah transaksi pengeluaran untuk kategori $kategori: ${expenseTransactions.length}');
+
+  if (expenseTransactions.isEmpty) {
+    return null;
+  }
+
+  expenseTransactions.sort((a, b) => a.jumlah.compareTo(b.jumlah));
+
+  print('Transaksi setelah pengurutan: ${expenseTransactions.map((t) => t.jumlah)}');
+
+  return expenseTransactions.first;
+}
 
   void clearTransaksi() {
     _transaksi.clear();
